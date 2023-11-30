00d622df1c5033f546da330202bdb94892de34bc
Kyle <kylemcguire35@gmail.com>
2023-11-29 21:14:11 -0800

    completed intersection

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,26 @@
-function intersection(a, b) {}
+/*
+initialize a new Map
+result = []
+for each element e of a do:
+  add e to the Map
+
+for each element e of b do:
+  lookup e in the Map
+  if e is in the Map then
+    add e to the result array
+return result
+*/
+
+function intersection(a, b) {
+  const aSet = new Set(a);
+  const bSet = new Set(b);
+  let result = [];
+  aSet.forEach((e) => {
+    if (bSet.has(e)) {
+      result.push(e);
+    }
+  });
+  return result;
+}
 
 module.exports = intersection;
