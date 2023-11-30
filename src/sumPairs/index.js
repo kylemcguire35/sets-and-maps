62f810ea9b20d5446f506b1625b7a977f8da4afe
Kyle <kylemcguire35@gmail.com>
2023-11-29 21:29:41 -0800

    completed sum pairs

--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,41 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+
+/*
+inputs: an array of numbers named A
+        a target value named N
+
+initialize a new Map named numbers
+initialize a new Map named solution
+
+for each element e in A do:
+   add e to numbers
+
+for each element e in A do;
+  calculate diff = N - e
+  if diff is not equal to e then  (1)
+     look up diff in numbers
+     if diff is in numbers then
+       add [e, diff] to solution (2)
+
+convert solution to an array and return it
+*/
+
+function sumPairs(A, N) {
+  const numbers = new Set();
+  const solution = new Set();
+
+  A.forEach((e) => {
+    numbers.add(e)
+    const diff = N - e;
+    if (diff !== e) {
+      if (numbers.has(diff)) {
+        solution.add([e, diff]);
+      }
+    }
+  });
+  return Array.from(solution);
+}
 
 module.exports = sumPairs;
